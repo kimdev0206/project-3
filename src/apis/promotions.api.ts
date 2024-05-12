5a1c6be8a20d381b3baa0f26e21b6adf85d5bd52
Yongki Kim <kimdev0206@gmail.com>
2024-05-12 15:18:54 +0900

    add: 프로모션 api 연동

--- src/apis/promotions.api.ts
+++ src/apis/promotions.api.ts
@@ -1,21 +1,14 @@
 import { IPromotion } from "../models/promotion.model";
 
 export default class PromotionsAPI {
+  static url = process.env.REACT_APP_BASE_URL + "/promotions";
+
   static async getPromotions() {
-    const data: IPromotion[] = [
-      {
-        id: 1,
-        title: "IT 카테고리 도서",
-        discountRate: 0.3,
-        startAt: "2024-05-01",
-        endAt: "2024-05-31",
-      },
-      {
-        id: 2,
-        title: "구글 이메일 회원",
-        discountRate: 0.1,
-      },
-    ];
+    const response = await fetch(this.url, {
+      method: "GET",
+    });
+
+    const { data }: { data: IPromotion[] } = await response.json();
     return data;
   }
 }
